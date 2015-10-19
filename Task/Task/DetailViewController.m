2b22925266b41c47c8f5ac452ea9d0b97c2bdb3e
Ionenko Evgenii <ionenko.e.a@gmail.com>
2015-10-19 10:29:51 +0300

    Add link to Button

--- Task/Task/DetailViewController.m
+++ Task/Task/DetailViewController.m
@@ -6,22 +6,34 @@
 //  Copyright (c) 2015 Evgeny Ionenko. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "DetailViewController.h"
 
-@interface ViewController ()
+@interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textFiel;
+@property (weak, nonatomic) IBOutlet UIDatePicker *FatePicker;
+
+@property (weak, nonatomic) IBOutlet UIButton *ButtonSave;
+
+
+
 
 @end
 
-@implementation ViewController
+@implementation DetailViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.ButtonSave addTarget:self action:@selector(save) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) save{
+    NSLog(@"Save...");
 }
 
 @end
